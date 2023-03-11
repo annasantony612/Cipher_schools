7ea2ddd7604163bd637094e2ac95afee85c84b83
annasantony612 <annasantony612@gmail.com>
2023-03-11 14:19:45 +0530

    first commit

--- cipher_schools/src/components/Firstpg.js
+++ cipher_schools/src/components/Firstpg.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+import SearchIcon from "@mui/icons-material/Search";
 import { Box } from "@mui/material";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
@@ -7,8 +9,18 @@ export class Firstpg extends Component {
   render() {
     return (
       <div className="Firstpg">
-        <div className="searchbox"></div>
-        {/* <Box color="black">Greetings from GeeksforGeeks!</Box> */}
+        <div className="searchbox">
+          <input
+            className="input"
+            type="text"
+            // id="message"
+            // name="message"
+            // onChange={handleChange}
+            // value={message}
+          />
+
+          {/* <svg data-testid="SearchIcon"></svg> */}
+        </div>
       </div>
     );
   }
